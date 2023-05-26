0b15264385756db34ccdd13eed5ae73dc04244bf
RusulAzomSumon <18103020@iubat.edu>
2023-05-26 23:54:50 +0600

    index page update

--- wp-content/themes/rasfolio/index.php
+++ wp-content/themes/rasfolio/index.php
@@ -1,6 +1,10 @@
 <!-- geting header -->
 <?php get_header(); ?>
+
+<div class="body">
 <P>this is rasfolio index</P>
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias numquam corrupti doloremque nam doloribus culpa inventore accusamus omnis distinctio minima blanditiis soluta nemo sequi sapiente molestiae quidem eum, quaerat enim nesciunt quibusdam rerum! Nostrum, quia tempore voluptatem eaque quasi ducimus voluptatum quo quam. Assumenda rem voluptatem molestias exercitationem, explicabo perferendis?
+
+</div>
 <!-- geting footer -->
 <?php get_footer(); ?>
